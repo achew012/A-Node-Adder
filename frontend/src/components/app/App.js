333fd0e91a820dfb9c1b4c2be122118ca8506286
achew012 <achew012@e.ntu.edu.sg>
2022-02-23 01:57:20 +0800

    troubleshooting audio secure context

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -6,7 +6,7 @@ import {
     BrowserRouter as Router,
     Route,
     Switch
-  } from 'react-router-dom';
+} from 'react-router-dom';
 
 import Homepage from '../common/Homepage';
 import Header from '../common/Header';
@@ -14,18 +14,19 @@ import ProjectManager from '../common/ProjectManager';
 import Project from '../common/Project';
 // import AnnotatorManager from '../common/AnnotatorManager';
 import Annotate from '../common/Annotate';
+
 function App() {
     return (
         <div className="App">
             <Router>
                 <Header />
                 <Switch>
-                    <Route exact path="/" component={Homepage}/>
+                    <Route exact path="/" component={Homepage} />
                     {/* <Route exact path="/annotators" component={AnnotatorManager}/> */}
                     {/* <Route exact path="/upload" component={Upload}/> */}
-                    <Route exact path="/projects" component={ProjectManager}/>
-                    <Route exact path="/project" component={Project}/>
-                    <Route exact path="/annotate" component={Annotate}/>
+                    <Route exact path="/projects" component={ProjectManager} />
+                    <Route exact path="/project" component={Project} />
+                    <Route exact path="/annotate" component={Annotate} />
                 </Switch>
             </Router>
         </div>
